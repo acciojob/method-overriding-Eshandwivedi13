dab5c614c970a270026dbed76334748333437b17
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-12-08 15:16:57 +0530

    third

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,16 +2,8 @@ package com.driver;
 
 public class Main {
   public static void main(String args[]){
-      A obj1 = new A();
-      obj1.meth();
-      System.out.println(obj1.meth());
-
-      B obj2 = new B();
-      obj2.meth();
-      System.out.println(obj2.meth());
-
-      A obj3 = new B();
-      System.out.println(obj3.meth());
+      A obj = new B();
+      obj.meth();
 
 
   }
