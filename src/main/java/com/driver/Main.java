6196291bc8d848ee7734257799ccc86262c33f20
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-12-08 14:54:55 +0530

    Second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,22 +2,24 @@ package com.driver;
 
 public class Main {
   public static void main(String args[]){
-      A obj = new B();
-      System.out.println(obj.meth());
+      A obj1 = new A();
+      obj1.meth();
+
+      B obj2 = new B();
+      obj2.meth();
+
+      A obj3 = new B();
+      System.out.println(obj3.meth());
 
 
   }
 }
 class A{
     String meth(){
-        return "";
+        return "returning a string";
     }
 }
 class B extends A{
-
-    B(){
-        super();
-    }
     String meth(){
         return "Method is overridden in Extendend class B";
     }
