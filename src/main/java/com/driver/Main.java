19f7a4b17dae9cfbf0b5408b204eb6254d3cdad4
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-12-08 15:31:20 +0530

    Sixth

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,38 @@
 package com.driver;
 
+//public class Main {
+//  public static void main(String args[]){
+//      A obj = new B();
+//      obj.meth();
+//  }
+//   public static class A{
+//        public String meth(){
+//            return "Invoking method from class A";
+//        }
+//    }
+//    public static class B extends A {
+//        // Class B is now a subclass of A
+//        @Override
+//        public String meth() {
+//            return "Method is overridden in Extended class B";
+//        }
+//    }
+//}
+
 public class Main {
-  public static void main(String args[]){
-      A obj = new B();
-      obj.meth();
-  }
-   public static class A{
-        public String meth(){
+    public static class A {
+        public String meth() {
             return "Invoking method from class A";
         }
     }
     public static class B extends A {
-        // Class B is now a subclass of A
         @Override
         public String meth() {
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
+    }
+    public static void main(String[] args) {
+        B b = new B();
+        b.meth();
     }
 }
